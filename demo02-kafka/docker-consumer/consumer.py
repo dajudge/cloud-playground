d796143cf8559d8a3215a44833b8dcbab8feee5d
Alex Stockinger <mail@alexstockinger.de>
2019-06-04 11:26:20 +0000

    Configurable kafka broker

--- demo02-kafka/docker-consumer/consumer.py
+++ demo02-kafka/docker-consumer/consumer.py
@@ -1,12 +1,13 @@
+import os
 from kafka import KafkaConsumer
 from json import loads
 
-bootstrap_servers='kafka-cp-kafka.kafka.svc:9092'
+bootstrap_servers=os.environ['BOOTSTRAP_SERVERS'].split(',')
 
-print('Starting kafka consumer: ' + bootstrap_servers)
+print('Starting kafka consumer: ' + ",".join(bootstrap_servers))
 consumer = KafkaConsumer(
     'numtest',
-     bootstrap_servers=[bootstrap_servers],
+     bootstrap_servers=bootstrap_servers,
      auto_offset_reset='earliest',
      enable_auto_commit=True,
      group_id='demo02-group',
--- demo02-kafka/docker-producer/producer.py
+++ demo02-kafka/docker-producer/producer.py
@@ -1,11 +1,12 @@
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
+import os
 
-bootstrap_servers='kafka-cp-kafka.kafka.svc:9092'
+bootstrap_servers=os.environ['BOOTSTRAP_SERVERS'].split(',')
 
-print('Starting kafka producer: ' + bootstrap_servers)
-producer = KafkaProducer(bootstrap_servers=[bootstrap_servers], value_serializer=lambda x: dumps(x).encode('utf-8'))
+print('Starting kafka producer: ' + ",".join(bootstrap_servers))
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers, value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 for e in range(60):
     data = {'number' : e}
